039802cc1bc64e532d430fa8748915df0bde0007
Muhammad Ahsan Khan <79780732+Ahsan-khan41@users.noreply.github.com>
2022-03-18 11:14:23 +0500

    .

--- src/Components/ProductDetails/ProductCarousel.js
+++ src/Components/ProductDetails/ProductCarousel.js
@@ -1,11 +1,12 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Box } from "@mui/material";
+import './carousel.css'
 
 export default function ProductCarousel({ productCarouselList }) {
   return (
     <>
-      <Carousel>
+      <Carousel showStatus={false}>
         {productCarouselList.map((item, index) => (
           <Box key={index}>
             <img alt={index} src={item} />
